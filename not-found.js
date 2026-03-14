24e44c14404ec74ed0bb1a72b9f55e6c1975a5ec
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-14 23:00:21 +0530

    Add files via upload

---
+++ not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+            <h1>services Page not found!</h1>
+            <p>Could not find the page you are looking for</p>
+        </>
+    )
+}
