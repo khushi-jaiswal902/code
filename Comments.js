7b4c21f54c09eab51a1463d1526b20fbee839749
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-18 23:35:23 +0530

    Add files via upload

---
+++ Comments.js
@@ -0,0 +1,5 @@
+export default async function Comments() {
+    //await new Promise((resolve) => setTimeout(resolve, 8000))
+    console.log("Comments Component");
+    return <div>500 comments</div>
+}
