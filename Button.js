c8b1406e1e159fc5206e9d8d9a011c85e260962d
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-18 23:35:01 +0530

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,10 @@
+"use client"
+export default function Button() {
+  return (
+    <button onClick={() => {
+          console.log("Button Clicked");
+        }}>
+            Click Me 
+        </button>
+  )
+}
