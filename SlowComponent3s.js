e6dd58f911a39e746045815bc8a4d2ca2b805489
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-20 23:49:46 +0530

    Add files via upload

---
+++ SlowComponent3s.js
@@ -0,0 +1,11 @@
+
+export default async function SlowComponent3s() {
+    const response = await fetch("https://procodrr.vercel.app/?sleep=3000")
+      const data = await response.json();
+    
+  return (
+    <div>
+    {JSON.stringify(data)}
+    </div>
+  )
+}
