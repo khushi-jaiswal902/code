0947a0a7bfe56beb7275eb3000386086694c869e
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-21 13:53:23 +0530

    Add files via upload

---
+++ Views.js
@@ -0,0 +1,9 @@
+export default async function Views() {
+  //await new Promise ((resolve) => setTimeout(resolve, 3000))
+  console.log("Views Component");
+  return (
+    <div>
+        10k views
+    </div>
+  )
+}
