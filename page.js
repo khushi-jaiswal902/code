f19a607b0e3d8988830b35057c7a10e43a6bd6d3
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-22 22:43:33 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,8 +1,39 @@
+import Link from "next/link";
 
-function Blog1() {
-    return (
-        <div>page</div>
-    )
-}
+const About = () => {
+  console.log('About Page');
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link active">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>About Us</h1>
+        <p>We are a company dedicated to providing quality services.</p>
+      </div>
+    </>
+  );
+};
 
-export default Blog1
+export default About;
