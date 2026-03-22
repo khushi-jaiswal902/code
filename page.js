c6255153ecdd410abf3d7c975e46025a8035dad6
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-22 22:43:58 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,7 +1,15 @@
-import Link from "next/link";
+"use client "
 
-const About = () => {
-  console.log('About Page');
+import Link from "next/link";
+import Views from "../../../components/Views";
+import Likes from "../../../components/Likes";
+import Comments from "../../../components/Comments";
+import { Suspense } from "react";
+import Loading from "../../../components/Loading";
+import Button from "../../../components/Button";
+
+const Blogs = () => {
+  console.log("Blogs Page");
   return (
     <>
       <nav>
@@ -12,7 +20,7 @@ const About = () => {
             </Link>
           </li>
           <li>
-            <Link href="/about" className="nav-link active">
+            <Link href="/about" className="nav-link">
               About
             </Link>
           </li>
@@ -22,18 +30,33 @@ const About = () => {
             </Link>
           </li>
           <li>
-            <Link href="/blogs" className="nav-link">
+            <Link href="/blogs" className="nav-link active">
               Blogs
             </Link>
           </li>
         </ul>
       </nav>
       <div>
-        <h1>About Us</h1>
-        <p>We are a company dedicated to providing quality services.</p>
+        <h1>Welcome to Our Blog</h1>
+        <Button />
+        {/*<ol className="blog-links">
+          <li>
+            <Link href="/blogs/1">Blog 1</Link>
+          </li>
+          <li>
+            <Link href="/blogs/2">Blog 2</Link>
+          </li>
+          <li>
+            <Link href="/blogs/3">Blog 3</Link>
+          </li>
+        </ol>*/}
+        <br />
+          <Likes />
+          <Comments/>
+          <Views />
       </div>
     </>
   );
 };
 
-export default About;
+export default Blogs;
