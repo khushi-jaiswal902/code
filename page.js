e52f08a56b2577d5f3dc056c864be363c7c5f8c6
Khushi Jaiswal <khushi377689@gmail.com>
2026-03-31 18:42:06 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,15 +1,28 @@
 import Header from "../../../vite-project/src/components/Header";
+import ServiceItem from "../../../vite-project/src/components/ServiceItem";
+import ServiceList from "../../../vite-project/src/components/ServiceList";
 
-const About = () => {
+const Services = () => {
+  const services = [
+    "Web Development",
+    "Mobile App Development",
+    "Consulting Services",
+    "Digital Marketing",
+  ];
+
   return (
     <>
       <Header />
       <div>
-        <h1>About Us</h1>
-        <p>We are a company dedicated to providing quality services.</p>
+        <h1>Our Services</h1>
+        <ServiceList>
+          {services.map((service) => (
+            <ServiceItem key={service} serviceName={service} />
+          ))}
+        </ServiceList>
       </div>
     </>
   );
 };
 
-export default About;
+export default Services;
