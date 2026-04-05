f142442225db82d51f6c7d950b28b6e1cfea4fa9
Khushi Jaiswal <khushi377689@gmail.com>
2026-04-05 14:14:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Navbar from  './component/navbar'
+import Intro from "./component/Intro";
+function App() {
+  return(
+  <div className="App">
+    <Navbar/>
+     <Intro/>
+    </div>
+
+  )
+}
+export default App;
