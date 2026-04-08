6deb4849339657dcc400fa384d2328c26ebb56b2
Khushi Jaiswal <khushi377689@gmail.com>
2026-04-08 23:57:33 +0530

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,27 @@
+"""data abstraction - hide complex implementation details"""
+
+"""from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass #no implementation
+
+class Car(Vehicle):
+    def start(self):
+        print('car start with a key')
+
+class Bike(Vehicle):
+    def start(self):
+        print('Bike start with a button')
+
+car = Car()
+bike = Bike()
+
+car.start()
+bike.start() """
+
+
+"""type of errors:- 
+1.compile time error
+2.runtime error
+3.logical error"""
